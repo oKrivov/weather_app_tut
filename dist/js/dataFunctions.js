d0420e1a33fed13081488398147973e2aa3dfa9c
oKrivov <krivovoleg23@gmail.com>
2022-02-02 13:38:00 +0300

    copy from Git

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -9,20 +9,20 @@ export const setLocationObject = (locationObj, coordsObj) => {
 };
 
 export const getHomeLocation = () => {
-	return localStorage.getItem('defaultWeatherLocation');
+	return localStorage.getItem("defaultWeatherLocation");
 };
 
 export const getWeatherFromCoords = async (locationObj) => {
-	/*const lat = locationObj.getLat();
+	/* const lat = locationObj.getLat();
 	const lon = locationObj.getLon();
 	const units = locationObj.getUnit();
-	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&	exclude=minutely,hourly,alert&units=${units}&appid=${WEATER_API_KEY}`;
+	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
 	try {
-		const weatherStream = await fetch(url);
-		const weatherJson = await weatherStream.json();
-		return weatherJson;
+	  const weatherStream = await fetch(url);
+	  const weatherJson = await weatherStream.json();
+	  return weatherJson;
 	} catch (err) {
-		console.error(err);
+	  console.error(err);
 	} */
 
 	const urlDataObj = {
@@ -31,9 +31,9 @@ export const getWeatherFromCoords = async (locationObj) => {
 		units: locationObj.getUnit()
 	};
 	try {
-		const weatherStream = await fetch('./.netlify/functions/get_weather', {
+		const weatherStream = await fetch("./.netlify/functions/get_weather", {
 			method: "POST",
-			body: JSON.stringify(urlDataObj),
+			body: JSON.stringify(urlDataObj)
 		});
 		const weatherJson = await weatherStream.json();
 		return weatherJson;
@@ -45,27 +45,27 @@ export const getWeatherFromCoords = async (locationObj) => {
 export const getCoordsFromApi = async (entryText, units) => {
 	/* const regex = /^\d+$/g;
 	const flag = regex.test(entryText) ? "zip" : "q";
-	const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATER_API_KEY}`;
+	const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
 	const encodedUrl = encodeURI(url);
 	try {
-		const dataStream = await fetch(encodedUrl);
-		const jsonData = await dataStream.json();
-		return jsonData;
+	  const dataStream = await fetch(encodedUrl);
+	  const jsonData = await dataStream.json();
+	  return jsonData;
 	} catch (err) {
-		console.error(err.stack);
+	  console.error(err.stack);
 	} */
 
 	const urlDataObj = {
 		text: entryText,
-		units: units,
+		units: units
 	};
 	try {
-		const dataStream = await fetch('./.netlify/functions/get_coords', {
+		const dataStream = await fetch("./.netlify/functions/get_coords", {
 			method: "POST",
 			body: JSON.stringify(urlDataObj)
 		});
 		const jsonData = await dataStream.json();
-		return jsonData; 
+		return jsonData;
 	} catch (err) {
 		console.error(err);
 	}
@@ -73,6 +73,6 @@ export const getCoordsFromApi = async (entryText, units) => {
 
 export const cleanText = (text) => {
 	const regex = / {2,}/g;
-	const entryText = text.replaceAll(regex, ' ').trim();
+	const entryText = text.replaceAll(regex, " ").trim();
 	return entryText;
-}
+};
